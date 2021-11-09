cc40aecc610415794d6959de6c8664668326c9a0
Bruno Inácio <brunoinaciosouto40@gmail.com>
2021-11-09 16:17:49 -0300

    Alteração de estrutura de pastas do projeto

---
+++ Workers/Transportadora/Worker.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using RabbitLib;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Transportadora
+{
+    public class Worker : BackgroundService
+    {
+        private readonly RabbitManager _rabbitManager;
+        private readonly IModel _channel;
+
+        private readonly RabbitConfig _rabbitConfig;
+
+        public Worker(IConfiguration config)
+        {
+            _rabbitConfig = config.GetSection("RabbitConfig").Get<RabbitConfig>();
+            _rabbitManager = new RabbitManager(_rabbitConfig);
+            _channel = _rabbitManager.CreateChannel(_rabbitConfig.Queue);
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            var consumer = new EventingBasicConsumer(_channel);
+
+            consumer.Received += (sender, eventArgs) =>
+            {
+                var content = eventArgs.Body.ToArray();
+                var message = Encoding.UTF8.GetString(content);
+
+                Console.WriteLine($"Iniciando preparação do código de rastreio e envio do produto para ponto de coleta");
+                _channel.BasicAck(eventArgs.DeliveryTag, false);
+
+                ///
+                Thread.Sleep(10000); //Simula as validações e processamento
+                ///
+
+                Console.WriteLine($"Processo finalizado - {message}");
+
+                FinalizaProcessamentoDoPedido(message);
+            };
+
+            _channel.BasicConsume(_rabbitConfig.Queue, false, consumer);
+
+            return Task.CompletedTask;
+        }
+
+        private void FinalizaProcessamentoDoPedido(string message)
+        {
+            Console.WriteLine($"{message} enviado para transportadora e fim do ciclo principal do pedido");
+        }
+    }
+}
